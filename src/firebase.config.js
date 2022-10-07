208131cacf33d8fc561d056f32614a9ad93e02ae
Furkan Çetinkaya <furkancetinkaya0746@gmail.com>
2022-10-07 20:29:46 +0300

    add fbsconfig

---
+++ src/firebase.config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyA7ezPQi3dHKBgQI1gIsWCyS8BLxLMu8pI",
+        authDomain: "honeylink-a9d4c.firebaseapp.com",
+    projectId: "honeylink-a9d4c",
+    storageBucket: "honeylink-a9d4c.appspot.com",
+    messagingSenderId: "374781931923",
+    appId: "1:374781931923:web:f377098afe9b3701243866",
+    measurementId: "G-EGQ7MY4VLS"
+};
